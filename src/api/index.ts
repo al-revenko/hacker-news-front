2d1c1d7db29f54af04868c6a205a6bf50dea840c
Alexander Revenko <revenko.a@bk.ru>
2024-07-19 16:28:32 +0700

    feat: api функция getArticlesList

---
+++ src/api/index.ts
@@ -0,0 +1,24 @@
+import { FeedItem, ListArticlesType } from './types';
+import { articlesPerPage, axiosInst, maxPageCount } from './config';
+
+export async function getArticlesList(category: ListArticlesType, count: number) {
+  if (count <= 0) {
+    return [];
+  }
+
+  const argsPageCount = count >= articlesPerPage ? Math.ceil(count / articlesPerPage) : 1;
+
+  const pageCount = argsPageCount <= maxPageCount[category] ? argsPageCount : maxPageCount[category];
+
+  const feedList: FeedItem[] = [];
+
+  for (let page = 1; page <= pageCount; page++) {
+    const req = await axiosInst.get<FeedItem[]>(`${category}/${page}.json`);
+
+    if (req.status === 200 && Array.isArray(req.data)) {
+      feedList.push(...req.data);
+    }
+  }
+
+  return feedList.slice(0, count);
+}
