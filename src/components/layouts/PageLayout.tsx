6c7d69889729e038f334cf50183c9955768b35a0
Alexander Revenko <revenko.a@bk.ru>
2024-07-20 16:02:54 +0700

    refactor: обновил PageLayout

--- src/components/layouts/PageLayout.tsx
+++ src/components/layouts/PageLayout.tsx
@@ -1,9 +1,17 @@
+import { styled } from '@mui/material';
 import { PropsWithChildren } from 'react';
+import COLORS from '~/const/colors';
 
 interface Props extends PropsWithChildren {}
 
 const PageLayout = ({ children }: Props) => {
-  return <div>{children}</div>;
+  return <Content>{children}</Content>;
 };
+
+const Content = styled('main')`
+  min-height: 100dvh;
+  width: 100%;
+  background-color: ${COLORS.secondary};
+`;
 
 export default PageLayout;
