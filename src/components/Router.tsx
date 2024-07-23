0b26b6f67261efac7df158a38c6740e7117e844f
Alexander Revenko <revenko.a@bk.ru>
2024-07-23 20:32:17 +0700

    refactor: обновил страницу 404

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -14,6 +14,10 @@ const routes: RouteObject[] = [
     element: <Article />,
   },
   {
+    path: ROUTES.NOTFOUND,
+    element: <NotFound />,
+  },
+  {
     path: '*',
     element: <NotFound />,
   },
--- src/components/pages/NotFound.tsx
+++ src/components/pages/NotFound.tsx
@@ -1,11 +1,15 @@
-import PageLayout from '~/components/layouts/PageLayout';
 import ROUTES from '~/const/routes';
+import PageLayout from '~/components/layouts/PageLayout';
 import LinkBack from '~/components/ui/links/LinkBack';
+import Heading from '~/components/ui/heading/Heading';
+import ContentLayout from '~/components/layouts/ContentLayout';
 
 const NotFound = () => {
   return (
-    <PageLayout headerProps={{ leftChild: <LinkBack to={ROUTES.HOME} /> }}>
-      <h1>Not Found</h1>
+    <PageLayout headerProps={{ leftChild: <LinkBack to={ROUTES.HOME} color="secondary" /> }}>
+      <ContentLayout>
+        <Heading variant="h1">404: NOT FOUND</Heading>
+      </ContentLayout>
     </PageLayout>
   );
 };
