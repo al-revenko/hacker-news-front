38e3d915ffff898ca828098511cec33429667322
Alexander Revenko <revenko.a@bk.ru>
2024-08-01 20:45:04 +0700

    refactor: удалил LinkPage

--- src/components/ui/links/LinkPage.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonProps } from '@mui/material';
-import { LinkProps } from './linkProps.type';
-import ButtonPage from '~/components/ui/buttons/ButtonPage';
-
-const LinkPage = ({ to, color }: Pick<ButtonProps, 'color'> & LinkProps) => {
-  return <ButtonPage component={'a'} href={to} size="large" color={color} />;
-};
-
-export default LinkPage;
